d497418afc8ce5f87768ff17657f7908ebf74476
elghrbawy <mohamed.elgharbawy@cyshield.com>
2023-10-24 19:23:51 +0300

    oyster user CRUD

--- managers/entities/user/User.manager.js
+++ managers/entities/user/User.manager.js
@@ -1,41 +1,90 @@
-module.exports = class User {
+module.exports = class Student {
   constructor({
     utils,
     cache,
     config,
     cortex,
+    oyster,
     managers,
     validators,
-    mongomodels,
   } = {}) {
     this.config = config;
     this.cortex = cortex;
     this.validators = validators;
-    this.mongomodels = mongomodels;
+    this.oyster = oyster;
+    this.utils = utils;
     this.tokenManager = managers.token;
-    this.oyster = managers.oyster;
-    this.usersCollection = "users";
-    this.httpExposed = ["createUser"];
+    this.collection = "user";
+    this.httpExposed = [
+      "createUser",
+      "get=getUser",
+      "put=updateUser",
+      "delete=removeUser",
+    ];
   }
 
-  async createUser({ username, email, password }) {
-    const user = { username, email, password };
+  async createUser(user) {
+    // const user = { username, email, password };
 
     // Data validation
-    let result = await this.validators.user.createUser(user);
-    if (result) return result;
+    const validationError = await this.validators.user.createUser(user);
+
+    if (validationError) return validationError;
 
     // Creation Logic
-    let createdUser = { username, email, password };
-    let longToken = this.tokenManager.genLongToken({
-      userId: createdUser._id,
-      userKey: createdUser.key,
+    const userId = Math.random().toString(36).substr(2);
+    const role = "school_admin";
+    const newUser = {
+      _id: "userId",
+      role,
+      username,
+      email,
+      password,
+    };
+
+    const savedUser = await this.oyster.call("add_block", {
+      // relation: this.collection,
+      _label: this.collection,
+      ...newUser,
+    });
+
+    const longToken = this.tokenManager.genLongToken({
+      userId: savedUser._id,
+      role: savedUser.role,
     });
 
     // Response
     return {
-      user: createdUser,
+      user: savedUser,
       longToken,
     };
+  }
+  // ----------------------------------------------------------------------------------------------
+  async getUser({ userId }) {
+    const user = await this.oyster.call("get_block", `user:userId`);
+    if (!user || this.utils.isEmpty(user)) return { error: "user not found" };
+    return user;
+  }
+  // ----------------------------------------------------------------------------------------------
+  async updateUser({ userId, username, email, password }) {
+    const validationError = await this.validators.user.createUser(user);
+
+    if (validationError) return validationError;
+    const user = await this.oyster.call("update_block", {
+      _id: `${this.collection}:${userId}`,
+      username,
+    });
+
+    return user;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+
+  async removeUser() {
+    const user = await this.oyster.call(
+      "delete_block",
+      `${this.collection}:userId`
+    );
+    return user;
   }
 };
